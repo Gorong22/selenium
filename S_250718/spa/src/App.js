e5a744a316f923f5c560f03b8599a8a3479ece95
Gorong22 <mare454545@gmail.com>
2025-07-18 18:08:54 +0900

    initial commit

---
+++ S_250718/spa/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import Viewer from "./Viewer";
+import Controller from "./Controller";
+import "./App.css";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const handleSetCount = (value) => {
+    setCount(count + value);
+  };
+  return (
+    <div className="App">
+      <h1>Simple Conter</h1>
+
+      <section>
+        <Viewer count={count} />
+      </section>
+
+      <section>
+        <Controller handleSetCount={handleSetCount} />
+      </section>
+    </div>
+  );
+};
+
+export default App;
